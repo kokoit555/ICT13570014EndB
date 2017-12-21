fb225cff6dfb8b70d2d3b959545d1ef7e142f523
13580024 <nutham024@hotmail.com>
2017-12-21 14:31:34 +0700

    Add project files.

---
+++ ICT13570014EndB/ICT13570014EndB/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ICT13570014EndB
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
